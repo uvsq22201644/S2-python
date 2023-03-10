5ae916f1f305b62866ac997532c9ac20ae781553
uvsq22201644 <andrea.cuvelier@ens.uvsq.fr>
2023-03-10 17:47:51 +0100

    cible.py

---
+++ TD/TD2/cible.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+WIDTH = 500
+HEIGHT = 500
+CERCLES = 50
+BG_COLOR = '#000000'
+
+if WIDTH <= HEIGHT :
+    SIZE = WIDTH/(CERCLES*2)
+else:
+    SIZE = HEIGHT/(CERCLES*2)
+
+couleurs =['blue','green','black','yellow','magenta','red']
+
+root =Tk()
+
+canvas= Canvas(root,width=WIDTH,height=HEIGHT,bg=BG_COLOR)
+canvas.pack()
+
+for i in range(CERCLES):
+    canvas.create_oval(SIZE*i,SIZE*i,WIDTH-SIZE*i,HEIGHT-SIZE*i,fill=couleurs[i%len(couleurs)],width=0)
+
+root.resizable(False,False)
+root.mainloop()
